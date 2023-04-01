aab2f7c39676eb56de376fa4a7ad2c7e2dbb7b09
Debak Roy <debakroy7@gmail.com>
2023-04-01 11:48:25 +0530

    new changes

--- js/jsFile.js
+++ js/jsFile.js
@@ -6,3 +6,7 @@ button1.addEventListener('click', () => {
     const url = `https://www.google.com/search?q=${encodeURIComponent(query)}`;
     window.location.href = url;
 })
+
+button2.addEventListener('click', () => {
+    window.location.href = `https://www.google.com/doodles`;
+})
